40c88c1928dd7f42dd8506da0cab0610fb724816
MI-NOTEBOOK-PRO\German <germfema@uax.es>
2022-11-27 12:26:59 +0100

    Commit inicial

---
+++ src/main/java/com/uax/accesoadatos/ProyectoPlantillaSpringMVC/ProyectoPlantillaSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.uax.accesoadatos.ProyectoPlantillaSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPlantillaSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPlantillaSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/uax/accesoadatos/ProyectoPlantillaSpringMVC/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.uax.accesoadatos.ProyectoPlantillaSpringMVC;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProyectoPlantillaSpringMvcApplication.class);
+	}
+
+}
